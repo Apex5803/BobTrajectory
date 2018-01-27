e6011954218712eab15c6470792b3a3fa0dcbb4d
Stephanie Morrison <stephanie.hoag@gmail.com>
2018-01-27 12:45:51 -0800

    FiveFeet trajectory for testing and tuning path following

--- src/com/team319/Main.java
+++ src/com/team319/Main.java
@@ -8,9 +8,10 @@ import com.team319.trajectory.SrxTranslatorConfig;
 
 /**
  * Forked from 254's 2014 Trajectory library just a comment to make a change
- * 
+ * Then forked from 319's Trajectory library.
  * @author Jared341
  * @author ttremblay
+ * @author Apex5803
  */
 public class Main {
 
@@ -21,22 +22,25 @@ public class Main {
 		//Standard configs between all trajectories
 		standardConfig.name = "StandardConfig";
 		standardConfig.dt = .01;
-		standardConfig.max_acc = 10.0;
+		standardConfig.max_acc = 5.00;
 		standardConfig.max_jerk = 60.0;
-		standardConfig.max_vel = 4.0; // gearbob was 6.0
+		standardConfig.max_vel = 4.0; // started at 4.0, // gearbob was 6.0
 		standardConfig.wheelbase_width_feet = 32.5/12.0;
 		standardConfig.wheel_dia_inches = 3.5;
-		standardConfig.scale_factor = 2.35; //0.899 // gearbob is 2.35
-		standardConfig.encoder_ticks_per_rev = 1024;
+		standardConfig.scale_factor = 2.0; //0.899 // gearbob is 2.35
+		standardConfig.encoder_ticks_per_rev = 256; //1024
 		
 		SrxTranslatorConfig slowConfig = new SrxTranslatorConfig(standardConfig);
-		slowConfig.max_vel = 4.0;
+		slowConfig.max_vel = 3.0 ; // 4.0;
 		
 		
 		
-		BobPath ThreeFeet = new BobPath(standardConfig, "ThreeFeet", 1);
-		ThreeFeet.addWaypoint(new WaypointSequence.Waypoint(0, 0, 0));
-		ThreeFeet.addWaypoint(new WaypointSequence.Waypoint(3.0, 0.0, Math.toRadians(0.0)));
+		BobPath FiveFeet = new BobPath(standardConfig, "FiveFeet", 1);
+		FiveFeet.addWaypoint(new WaypointSequence.Waypoint(0, 0, 0));
+		FiveFeet.addWaypoint(new WaypointSequence.Waypoint(5.0, 0.0, Math.toRadians(0.0)));
+		
+		
+		
 		
 		BobPath OneFoot = new BobPath(standardConfig, "OneFoot", 1);
 		OneFoot.addWaypoint(new WaypointSequence.Waypoint(0, 0, 0));
@@ -44,8 +48,8 @@ public class Main {
 		
 	
 
-				BobPathGenerator.exportPath("Paths", ThreeFeet);
-				BobPathGenerator.exportPath("Paths", OneFoot);
+				BobPathGenerator.exportPath("Paths", FiveFeet);
+				//BobPathGenerator.exportPath("Paths", OneFoot);
 			
 		
 		//BobPathGenerator.appendAndExportPaths("Paths", "appendedPath", false, blueHopperThenShootAutoLeftSidePt2, toAppend);
